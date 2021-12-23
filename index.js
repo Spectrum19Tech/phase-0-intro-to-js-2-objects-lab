484522428dea4685848fe16b51fb68066e815c7a
Jonathan Bryant <spectrum19tech@gmail.com>
2021-12-23 11:05:13 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Elon Musk",
+    streetAddress: "1234 Launch Blvd"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObject =   {...employee};
+    newObject[key] = value;
+    return newObject;
+} 
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newObject = {...employee};
+    delete newObject[key];
+    return newObject;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
